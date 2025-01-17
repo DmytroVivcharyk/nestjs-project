2f3821e33bdbe06ed1392075cf1275deded750ef
DimaDev <dmitrovivcarik@gmail.com>
2025-01-17 14:42:30 +0200

    Add swager, compodoc, conected postgres DB, intall TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
 
+const ConfiguredTypeOrmModule = TypeOrmModule.forRootAsync({
+  imports: [],
+  inject: [],
+  useFactory: () => ({
+    type: 'postgres',
+    entities: [],
+    host: 'localhost',
+    port: 9999,
+    username: 'nest_user',
+    password: 'nest_password',
+    database: 'nest_db',
+    autoLoadEntities: true, // Automatically load entities
+    synchronize: true, // Use only in development, not production
+  }),
+});
+
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [UsersModule, PostsModule, AuthModule, ConfiguredTypeOrmModule],
 })
 export class AppModule {}
--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,19 +1,37 @@
 import { Injectable, Inject, forwardRef } from '@nestjs/common';
 import { AuthService } from 'src/auth/providers/auth.service';
-
+/**
+ * UsersService to connect to Users table and perform buisned logic
+ * Inject AuthService to be able to use Authservice
+ */
 @Injectable()
 export class UsersService {
+  /**
+   * @Constructor
+   * @param authService
+   * Inject AuthService
+   */
   constructor(
     @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
   ) {}
 
+  /**
+   * The method that returns a string if use is authenticated
+   * Uses Authservice to check if user is authenticated
+   * @returns  string
+   */
   getHello(): string {
     const isAuthenticated = this.authService.isAuthenticated();
     if (!isAuthenticated) return 'Not authenticated';
     return 'Hello from users Injectable servise provider!';
   }
 
+  /**
+   * The method that returns a user by id
+   * @param userId string
+   * @returns user { userId: string; firstName: string; lastName: string }
+   */
   public getUserById(userId: string) {
     return {
       userId,
